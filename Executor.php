f79a3926cfcb4536296f0ace65194d9732cff238
Darrell Hamilton <darrellh@4over.com>
2012-02-23 11:03:59 -0800

    normalized indentation to 4 spaces and eliminated tabs.  Added header loading

--- Executor.php
+++ Executor.php
@@ -4,6 +4,7 @@ namespace Zeroem\RequestExecutorBundle;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\HeaderBag;
 
 /**
  * Utility class for parsing a Request object into a cURL request
@@ -13,68 +14,103 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class Executor
 {
-  static private $_methodOptionMap = array(
-    "GET"=>CURLOPT_GET,
-    "POST"=>CURLOPT_POST,
-    "HEAD"=>CURLOPT_NOBODY,
-    "PUT"=>CURLOPT_PUT
-  );
+    static private $_methodOptionMap = array(
+        "GET"=>CURLOPT_GET,
+        "POST"=>CURLOPT_POST,
+        "HEAD"=>CURLOPT_NOBODY,
+        "PUT"=>CURLOPT_PUT
+    );
 
-  /**
-   * Execute a Request object via cURL
-   *
-   * @param Request $request the request to execute
-   * @param array $options additional curl options to set/override
-   * @return Response
-   */
-  static public function execute(Request $request, array $options = array()) {
+    /**
+     * Execute a Request object via cURL
+     *
+     * @param Request $request the request to execute
+     * @param array $options additional curl options to set/override
+     * @return Response
+     */
+    static public function execute(Request $request, array $options = array()) {
 
-    $handle = curl_init($request->getUri());
+        $handle = curl_init($request->getUri());
 
-    curl_setopt($handle,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt_array(
+            $handle,
+            array(
+                CURLOPT_RETURNTRANSFER=>true,
+                CURLOPT_HTTPHEADER=>static::buildHeadersArray($request->headers)
+            )
+        );
 
-    static::setMethod($handle,$request);
+        static::setMethod($handle,$request);
 
-    if ("POST" === $request->getMethod()) {
-      static::setPostFields($handle, $request);
+        if ("POST" === $request->getMethod()) {
+            static::setPostFields($handle, $request);
+        }
+
+        // Provided Options should override interpreted options
+        curl_setopt_array($handle, $options);
+
+        $result = curl_exec($handle);
+        
+        $exception = static::getErrorException($handle);
+        curl_close($handle);
+        
+        if (false !== $exception) {
+            throw $exception;
+        }
+
+        return $result;
     }
 
-    // Provided Options should override interpreted options
-    curl_setopt_array($handle, $options);
+    static private function setPostFields(resource $handle, Request $request) {
+        $postfields = null;
+        $content = $request->getContent();
 
-    $result = curl_exec($handle);
-    curl_close($handle);
-  }
+        if (null !== $content) {
+            $postfields = $content;
+        } else if (count($request->getRequest()->all()) > 0) {
+            $postfields = $request->getRequest()->all();
+        }
 
-  static private function setPostFields(resource $handle, Request $request) {
-    $postfields = null;
-    $content = $request->getContent();
-
-    if (null !== $content) {
-      $postfields = $content;
-    } else if (count($request->getRequest()->all()) > 0) {
-      $postfields = $request->getRequest()->all();
+        curl_setopt($handle, CURLOPT_POSTFIELDS, $postfields);
     }
 
-    curl_setopt($handle, CURLOPT_POSTFIELDS, $postfields);
-  }
+    static private function setMethod(resource $handle, Request $request) {
+        $method = $request->getMethod();
 
-  static private function setMethod(resource $handle, Request $request) {
-    $method = $request->getMethod();
-
-    if(isset(static::$_methodOptionMap[$method])) {
-      curl_setopt($handle,static::$_methodOptionMap[$method],true);
-    } else {
-      curl_setopt($handle,CURLOPT_CUSTOMREQUEST,$method);
+        if (isset(static::$_methodOptionMap[$method])) {
+            curl_setopt($handle,static::$_methodOptionMap[$method],true);
+        } else {
+            curl_setopt($handle,CURLOPT_CUSTOMREQUEST,$method);
+        }
     }
-  }
 
-  static private errorCheckAndHandling(resource $handle) {
-      $error_no = curl_errno($handle);
+    static private function getErrorException(resource $handle) {
+        $error_no = curl_errno($handle);
 
-      if(0 !== $error_no) {
-          throw new CurlErrorException(curl_error($handle), $error_no);
-      }
-  }
-  
+        if (0 !== $error_no) {
+            return new CurlErrorException(curl_error($handle), $error_no);
+        }
+
+        return false;
+    }
+
+    static private function buildHeadersArray(HeaderBag $headerBag) {
+        $headers = array();
+
+        foreach($headerBag->all() as $header=>$value) {
+            $headers[] = makeHeaderName($header) . ": {$value}";
+        }
+
+        return $headers;
+    }
+
+    static private function makeHeaderName($str) {
+        $parts = explode("_",strtolower($str));
+        
+        foreach($parts as &$part) {
+            $part = ucfirst($part);
+        }
+
+        return implode("-",$parts);
+    }
 }
